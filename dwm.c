9dad00af66670ad3bd139e0a49cfa36020bfbb95
cheminacci <65716931+cheminacci@users.noreply.github.com>
2020-09-12 11:26:11 -0400

    Add files via upload

--- dwm.c
+++ dwm.c
@@ -1697,7 +1697,7 @@ tagmon(const Arg *arg)
 void
 tile(Monitor *m)
 {
-	unsigned int i, n, h, r, g = 0, mw, my, ty;
+	unsigned int i, n, h, g = 0, mw, my, ty;
 	Client *c;
 
 	for (n = 0, c = nexttiled(m->clients); c; c = nexttiled(c->next), n++);
@@ -1712,14 +1712,12 @@ tile(Monitor *m)
        for (i = 0, my = ty = m->gappx, c = nexttiled(m->clients); c; c = nexttiled(c->next), i++)
 
 		if (i < m->nmaster) {
-	               r = MIN(n, m->nmaster) - i;
 		       h = (m->wh - my) / (MIN(n, m->nmaster) - i) - m->gappx;
                        resize(c, m->wx + m->gappx, m->wy + my, mw - (2*c->bw) - m->gappx, h - (2*c->bw), 0);
                        if (my + HEIGHT(c) + m->gappx < m->wh)
                                my += HEIGHT(c) + m->gappx;
 
 		} else {
-			r = n - i; 
 			 h = (m->wh - ty) / (n - i) - m->gappx;
                          resize(c, m->wx + mw + m->gappx, m->wy + ty, m->ww - mw - (2*c->bw) - 2*m->gappx, h - (2*c->bw), 0);
                          if (ty + HEIGHT(c) + m->gappx < m->wh)
